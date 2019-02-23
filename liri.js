35ee8f3d1a6ea23edd7919242afb87df5aa5d0fa
aheckmann13 <45889653+aheckmann13@users.noreply.github.com>
2019-02-22 19:32:34 -0600

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 
 require("dotenv").config();
 
+var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var keys = require("./keys.js");
 var fs = require("fs");
@@ -10,7 +11,7 @@ var spotify = new Spotify(keys.spotify);
 var nodeArgs = process.argv;
 
 switch (nodeArgs[2]) {
-    case 'concer-this':
+    case 'concert-this':
         concertThis();
         break;
     case 'spotify-this-song':
@@ -27,16 +28,80 @@ switch (nodeArgs[2]) {
         break;
 }
 
+function concertThis() {
+
+    let artist = "";
+
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        if (i > 3 && i < nodeArgs.length) {
+            artist = artist + "+" + nodeArgs[i];
+        }
+        else {
+            artist += nodeArgs[i];
+        }
+    }
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bands.id;
+
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(function (response) {
+        for (i = 0; i < 4; i++) {
+            var output = ('* Venue Name: ' + response.data[i].venue.name +
+                '\n* Venue Location: ' + response.data[i].venue.city + ", " +
+                response.data[i].venue.country +
+                '\n* Date of Concert: ' + response.data[i].venue.datetime + '\n\n');
+
+            console.log(output);
+        }
+    });
+
+};
+
+
+function spotifyThis() {
+
+    let songName = "";
+
+    if (nodeArgs.length < 4) {
+        songName = "The Sign";
+    }
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+            songName = songName + "+" + nodeArgs[i];
+        }
+        else {
+            songName += nodeArgs[i];
+        }
+    }
+    console.log(songName)
+
+    spotify.search({ type: 'track', query: songName }).then(function (response) {
+        console.log(response);
+    })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+    // var output = ('* Artist(s): ' + response.data.artist +
+    // '\n* Song Name: ' + songName+
+    // '\n* Preview Link: ' + response.data.preview + 
+    // '\n* Album:' + response.data.album +'\n\n');
+
+    // console.log(output);
+}
+
+
 function movieThis() {
 
     let movieName = "";
 
-    if(nodeArgs.length<4){
-        movieName="Mr. Nobody";
+    if (nodeArgs.length < 4) {
+        movieName = "Mr. Nobody";
     }
     for (var i = 3; i < nodeArgs.length; i++) {
 
-       if (i > 3 && i < nodeArgs.length) {
+        if (i > 3 && i < nodeArgs.length) {
             movieName = movieName + "+" + nodeArgs[i];
         }
         else {
@@ -48,30 +113,59 @@ function movieThis() {
     console.log(queryUrl);
 
     axios.get(queryUrl).then(function (response) {
-        
+
         var output = ('* Title: ' + response.data.Title +
             '\n* Year: ' + response.data.Year +
             '\n* IMDB Rating: ' + response.data.imdbRating +
-            '\n* Rotten Tomatoes Rating: ' + response.data.Ratings[1]
+            '\n* Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value
     /*need to figure out how to pull this rating */ +
             '\n* Country: ' + response.data.Country +
             '\n* Language: ' + response.data.Language +
             '\n* Plot: ' + response.data.Plot + '\n* Actors' + response.data.Actors);
-            
-            console.log(output);
-            fs.appendFile('log.txt', output + '\n\n', function(err){
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    console.log('Content Added');
-                }
-            });
+        console.log(output);
     });
-
 };
 
+function doIt() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
+        if (error) {
+            return console.log(error);
+        }
+        //Print the contents of data
+        console.log(data);
+        //Split data into array and log array
+        var dataArr = data.split(',');
+        console.log(dataArr);
 
+        //use index of array values to run switch
+        switch (dataArr[0]) {
+            case 'concert-this':
+                concertThis();
+                break;
+            case 'spotify-this-song':
+                songName = dataArr[1]; 
+                spotifyThis();
+                break;
+            case 'movie-this':
+                movieThis(dataArr[1]);
+                break;
+            case 'do-what-it-says':
+                doIt(dataArr[1]);
+                break;
+            default:
+                console.log('Try again');
+                break;
+        }
 
+    });
+}
 
+    // fs.appendFile('log.txt', output + '\n\n', function (err) {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     else {
+    //         console.log('Content Added');
+    //     }
+    // });
